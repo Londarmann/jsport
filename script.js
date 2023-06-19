57f576224cf0c6dcc1ead5466ad758046b9cd90e
LukaLondaridzen <tactusglacies@gmail.com>
2023-06-19 17:30:10 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll(".nav-link");
+    links.forEach(link => {
+        link.addEventListener("click", clickHandler);
+    });
+
+    function clickHandler(e) {
+        e.preventDefault();
+        const href = this.getAttribute("href");
+        const offsetTop = document.querySelector(href).offsetTop;
+
+        scroll({
+            top: offsetTop,
+            behavior: "smooth"
+        });
+    }
+});
